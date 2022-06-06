059a3005d70061cbd0f6a931da37b827a3bbd5b8
sal marvel <96798879+ioumarvey@users.noreply.github.com>
2022-06-06 23:30:04 +0200

    cleanup time
    działające menu, lekka zmiana jego kodu, ogólne czyszczenie i porządki, większość rzeczy jest teraz jako funkcje w globalu, main jest teraz short and neat

--- main.c
+++ main.c
@@ -3,12 +3,13 @@
 #include <time.h>
 #include <stdio.h>
 #include <wchar.h>
+
     char map[55][31];
     int mapC[55][31] = { 0 };
 
 char* blockTypes[] = {"-",  "║", "═", "╚", "╗", "╝", "╔", "╣", "╠", "╦"};
 
-double desiredFPS = 6.0;
+double desiredFPS = 6.0; //jeżeli gra będzie za wolna to możemy sobie zwiększyć
 
 enum directions {
   dirNone,
@@ -18,10 +19,34 @@ enum directions {
   dirBottom
 };
 
+enum gameState {
+  stateMenu,
+  statePlaying,
+  stateHighScores,
+  stateChangeName,
+  stateQuit
+};
+
+int currentGameState = stateMenu;
+
+bool isPlaying = true;
+    int highscore=0;
+    char username[]="anon";
+
+WINDOW * win;
+    int height=31;
+    int width=55;
+    int start_x = 0;
+    int start_y=0;
+    int x = 27;
+    int y= 23;
+
 int plansza(){
+  
     start_color();
     init_pair(1,COLOR_BLUE, COLOR_BLACK);
     attron(COLOR_PAIR(1));
+
     for(size_t j = 0; j < 31; j++) {
       for(size_t i = 0; i < 55; i++) {
         if(map[i][j] >= '0' && map[i][j] <= '9') {
@@ -38,78 +63,174 @@ int plansza(){
 
 int menu()
 {
-    initscr();
-    noecho();
-    curs_set(0);
-    cbreak();
-    int height=40;
-    int width=55;
-    int start_x = 0;
-    int start_y=0;
-    
-    //printing menu options
-    WINDOW * menuwin = newwin(height, width, start_x, start_y);
-    box(menuwin, 0, 0);
-    keypad(menuwin, true);
+    wclear(win);
+//    erase();
     refresh();
-    wrefresh(menuwin);
-    mvwprintw(menuwin, 0, 16, " * * * PAC - MAN * * * ");
-    int highscore=0;
-    char username[]="anon";
-    mvwprintw(menuwin, 2, 2, "Highscore: %d", highscore);
-    mvwprintw(menuwin, 3, 2, "Player: %s", username);
+    box(win, 0, 0);
+//    keypad(win, true);
+    refresh();
+    wrefresh(win);
+    mvwprintw(win, 0, 16, " * * * PAC - MAN * * * ");
+    mvwprintw(win, 2, 2, "Highscore: %d", highscore);
+    mvwprintw(win, 3, 2, "Player: %s", username);
     int menuy=15;
     int menux=20;
-    char opt1[] = "CHANGE NICKNAME ";
-    char opt2[] = "PLAY ";
-    char opt3[] = "HIGH SCORES ";
-    
-    
-    int choice;
-    int highlight = menuy;
+    char* options[4] = {
+      "CHANGE NICKNAME ",
+      "PLAY ",
+      "HIGH SCORES ",
+      "QUIT "
+    };
+
+    int currentChoice = 0;
+
+    int choiceResults[] = {
+      stateChangeName,
+      statePlaying,
+      stateHighScores,
+      stateQuit
+    };
+
+    bool isPicking = true;
+
+    int c;
     move(menuy, menux-1);
-    while(1){
-        mvwprintw(menuwin, menuy, menux, "%s", opt1);
-        mvwprintw(menuwin, menuy+1, menux, opt2);
-        mvwprintw(menuwin, menuy+2, menux, "%s", opt3);
-        if(highlight == menuy){
-            wattron(menuwin, A_STANDOUT);
-            mvwprintw(menuwin, menuy, menux, "%s", opt1);
-            wattroff(menuwin, A_STANDOUT);
-        }else if(highlight == menuy+1){
-            wattron(menuwin, A_STANDOUT);
-            mvwprintw(menuwin, menuy+1, menux, "%s", opt2);
-            wattroff(menuwin, A_STANDOUT);
-        }else if(highlight == menuy+2){
-            wattron(menuwin, A_STANDOUT);
-            mvwprintw(menuwin, menuy+2, menux, "%s", opt3);
-            wattroff(menuwin, A_STANDOUT);
+    while(isPicking){
+        for(size_t i = 0; i < 4; i++) {
+          if(i == currentChoice) { 
+            wattron(win, A_STANDOUT);
+            mvwprintw(win, menuy + i, menux, "%s", options[i]);
+            wattroff(win, A_STANDOUT);
+          } else
+            mvwprintw(win, menuy + i, menux, "%s", options[i]);
         }
-        choice=wgetch(menuwin);
-        switch (choice)
+
+        c=wgetch(win);
+        switch (c)
         {
         case KEY_UP:
-            if (highlight == menuy) highlight = highlight+2;
-            else highlight--;
+            if(currentChoice == 0)
+              currentChoice + (4 - 1);
+            else
+              currentChoice--;
             break;
         case KEY_DOWN:
-            if (highlight == 17) highlight = highlight-2;
-            else highlight++;
+            if(currentChoice == (4 - 1))
+              currentChoice - (4 - 1);
+            else
+              currentChoice++;            
+            break;
+        case 10: //enter
+            currentGameState = choiceResults[currentChoice];
+            isPicking = false;
             break;
         default:
             break;
         }
     
-        wrefresh(menuwin);
-        if (choice == 10) break;
+        wrefresh(win);
     }
 
-    getch(); //wait for user's input (return int of char)
+//    getch(); //wait for user's input (return int of char)
     refresh(); //refresh screen to match the memory
 
 
     endwin(); //zamknij ncurses  
     return 0;
+}
+      int currentDir = dirNone;
+
+void mainGame() {
+          int ch;
+          ch = wgetch(win);
+          if(ch == 'q') {
+            currentGameState = stateMenu;
+          }
+          switch (ch)
+          {
+            case KEY_LEFT:
+              currentDir = dirLeft;
+              break;
+            case KEY_RIGHT:
+              currentDir = dirRight;
+              break;
+            case KEY_UP:
+              currentDir = dirTop;
+              break;
+            case KEY_DOWN:
+              currentDir = dirBottom;
+              break;
+            default:
+              break;
+          }
+
+          /* cały movement */
+
+          // variables for attempted moves
+
+          int attemptedX = 0;
+          int attemptedY = 0;
+
+          // sprawdzamy w jakim kierunku się poruszamy
+
+          switch(currentDir) {
+            case dirLeft:
+              attemptedX = -1;
+              break;
+            case dirRight:
+              attemptedX = 1;
+              break;
+            case dirTop:
+              attemptedY = -1;
+              break;
+            case dirBottom:
+              attemptedY = 1;
+              break;
+            //do nothing if not set, it's likely the default dirNone
+          }
+
+          /* sprawdzamy czy możemy się poruszać w spaces dictated by our X i Y */
+          if(mapC[x + attemptedX][y + attemptedY] != 0) {
+            /*are we moving into an X'd corridor?*/
+            if(mapC[x + attemptedX][y + attemptedY] == 2) {
+              /* check left and right sides of X for a corridor, if yes move there */
+              if(mapC[x + attemptedX + 1][y + attemptedY] == 0) {
+                x += attemptedX + 1;
+                y += attemptedY;
+              } else if(mapC[x + attemptedX - 1][y + attemptedY] == 0) {
+                x += attemptedX - 1;
+                y += attemptedY;
+              }
+            } else {
+              /* halt, abort, nie wolno sie ruszać!!!!!!!!!! */
+              currentDir = dirNone;
+            }
+          } else {
+            /* MOVE */
+            x += attemptedX;
+            y += attemptedY;
+          }
+
+          /* rysowańsko */
+          init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+          move(0, 0);
+          plansza();
+          wrefresh(win);
+          attron(COLOR_PAIR(2));
+          mvwprintw(win, y, x, "●"); //ᗣ?
+          attroff(COLOR_PAIR(2));
+          wmove(win,y,x);
+
+
+          /* Portal */
+          if (y==14){
+            if (x<=0){
+            x = 55-x;
+            } else if ((x>=54) && attemptedX==1){
+              x = 0;
+            }
+          }
+
 }
 
 int main()
@@ -140,8 +261,10 @@ int main()
         readX = 0;
         readY++;
       } else {  
-        if((c >= '0' && c <= '9') || c == 'X') {
+        if((c >= '0' && c <= '9')) {
           mapC[readX][readY] = 1;
+        } else if(c == 'X') {
+          mapC[readX][readY] = 2;
         }
         map[readX][readY] = c; 
         readX++;
@@ -159,123 +282,40 @@ int main()
     //keypad(stdscr, TRUE); //dodaj obsluge klawiszy specjalnych
 
 
-    int height=31;
-    int width=55;
-    int start_x = 0;
-    int start_y=0;
-    int x = 27;
-    int y= 23;
-    WINDOW * win = newwin(height, width, start_x, start_y);
+    win = newwin(height, width, start_x, start_y);
     keypad(win, true);
     nodelay(win, 1);
 
 
     
     wmove(win,y,x);
-    int ch;
     refresh();
-    int currentDir = dirNone;
+          curs_set(0);
 
 
 
 
-    while(true)  //jako 'q' możemy dać wejście do menu
+    while(isPlaying) 
     {
-    clock_t prevTime = clock();
+      clock_t prevTime = clock();  //clock_t jest z biblioteki <time.h>
 
-        ch = wgetch(win);
-        if(ch == 'q') {
+      switch(currentGameState) {
+        case stateMenu:
+          menu();
+          break;
+        case statePlaying:
+          mainGame();
+          break;
+        case stateQuit:
+          isPlaying = false;
           break;
         }
-        switch (ch)
-        {
-          case KEY_LEFT:
-            currentDir = dirLeft;
-            break;
-          case KEY_RIGHT:
-            currentDir = dirRight;
-            break;
-          case KEY_UP:
-            currentDir = dirTop;
-            break;
-          case KEY_DOWN:
-            currentDir = dirBottom;
-            break;
-          default:
-            break;
-        }
 
-        /* MOVE LOGIC */
-
-        // variables for attempted moves
-
-        int attemptedX = 0;
-        int attemptedY = 0;
-
-        // Check what direction we're supposed to be moving in
-
-        switch(currentDir) {
-          case dirLeft:
-            attemptedX = -1;
-            break;
-          case dirRight:
-            attemptedX = 1;
-            break;
-          case dirTop:
-            attemptedY = -1;
-            break;
-          case dirBottom:
-            attemptedY = 1;
-            break;
-          //do nothign if not set, it's likely the default dirNone
-        }
-
-        /* Check if we can move into the spaces dictated by our tried X & Y */
-        if(mapC[x + attemptedX][y + attemptedY] != 0) {
-          /* can't move! abort movement */
-          currentDir = dirNone;
-        } else {
-          /* MOVE */
-          x += attemptedX;
-          y += attemptedY;
-        }
-
-        /* BEGIN DRAWING */
-        curs_set(0);
-        init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-        move(0, 0);
-        plansza();
-        wrefresh(win);
-        attron(COLOR_PAIR(2));
-        mvwprintw(win, y, x, "●"); //ᗣ
-        attroff(COLOR_PAIR(2));
-        wmove(win,y,x);
-
-
-      /* Portal */
-        if (y==14){
-          if (x<=0){
-          x = 55-x;
-          } else if ((x>=54) && attemptedX==1){
-            x = 0;
-          }
-        }
-        
-        
-        
-        // if(x<=0 || x>=width || y<=0 || y>=height){
-        //   if((x <= 0 || x >= width) && y == 14) {
-        //     x = 55 - x; //loop
-        //   } else {
-        //     break;
-        //   }
-        // }
-
-        /* Hold the frame until we reach set amount of FPS */
+        /* czekamy aż max będzie dana amount of fps*/
         while(true) {
           clock_t curTime = clock();
           if( ( (double)curTime-(double)prevTime)/CLOCKS_PER_SEC > (1.0/desiredFPS))
-            break;
+            break; //liczenie klatek na sekunde
         }
         refresh(); //refresh screen to match the memory
     }
